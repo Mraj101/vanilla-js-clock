f0ddb41430f078ddbbbd53ae95312e16d28c0e11
Hasnain <mrajhasnain101@gmail.com>
2022-06-27 23:40:09 +0600

    bug fixed

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-a = new Date();
 let seconds;
 let minutes;
 let hours;
 let livetime;
-const time = document.querySelector("#times");
-setInterval(() => {  
- seconds = a.getSeconds();
- minutes = a.getMinutes();
- hours = a.getHours();
-if (hours > 12) hours = hours - 12;
- livetime = hours + " : " + minutes + " : " + seconds;
-time.innerHTML = livetime;
+var time = document.querySelector("#times");
+setInterval(() => {
+  a = new Date();
+  seconds = a.getSeconds();
+  minutes = a.getMinutes();
+  hours = a.getHours();
+  if (hours > 12) hours = hours - 12;
+  livetime = hours + " : " + minutes + " : " + seconds;
+  time.innerHTML = livetime;
 }, 1000);
