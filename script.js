5e3a3eaa6bf4cdd1fd7e0092c236a9af41ba0f9b
Hasnain <mrajhasnain101@gmail.com>
2022-08-11 21:29:59 +0600

    modified

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ let minutes;
 let hours;
 let livetime;
 let time = document.querySelector("#times");
-const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-setInterval(() => {
-    date = new Date();
-  day= date.tolocalDateString(undefined,options);
-  seconds = date.getSeconds();
-  minutes = date.getMinutes();
-  hours = date.getHours();
-  if (hours > 12) hours = hours - 12;
-  livetime = hours + " : " + minutes + " : " + seconds;
-  time.innerHTML = livetime+"on"+day;
-}, 1000);
+setInterval(()=>{
+a=new Date();
+hours=a.getHours();
+minutes=a.getMinutes();
+seconds=a.getSeconds();
+if(hours>12)
+{
+    hours=hours-12;
+}
+time.innerHTML=hours+":"+minutes+":"+seconds;
+},1000);
