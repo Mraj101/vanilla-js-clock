1319cfce6916af806476df527857b9f6f1b56070
Hasnain <mrajhasnain101@gmail.com>
2022-08-18 23:49:54 +0600

    modified

--- script.js
+++ script.js
@@ -13,31 +13,37 @@ const changeturn = ()=>{
         return "X";
 }
 
-
+// music.play();
 //check if x or 0 wins
 function chackwin()
 {
+    let boxtext=document.querySelectorAll(".boxtext");
     wins=[
-        [1,2,3],
-        [4,5,6],
-        [7,8,9],
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
         [1,4,7],
         [2,5,8],
-        [3,6,9],
-        [1,5,9],
-        [3,5,7]
+        [0,4,8],
+        [2,4,6],
     ];
-    wins.forEach(e=>{
-        let boxtext=element.querySelector(".boxtext");
-        if((boxtext.e[0]===boxtext.e[1])  &&  (boxtext.e[2]===boxtext.e[1])  &&  (boxtext.e[0]!==' '))
+    wins.forEach(e=>{ // the first   'e' in the array wins is [0,1,2] and e[0] is 0 and e[1] is 1
+        // so boxtext[e[0]] is equivalent to boxtext[0] which means , if you consider boxtext array there are nine spans in the html so there should be 
+
+        if( (boxtext[e[0]].innerText === boxtext[e[1]].innerText)  &&  (boxtext[e[2]].innerText === boxtext[e[1]].innerText)  &&  (boxtext[e[0]].innerText !== ''))
         {
+                document.querySelector('.info').innerText= boxtext[e[0]].innerText +" won";
+                // document.querySelector('.img').getElementsByTagName('img')[0].style.visibility="visible";
+
+                document.querySelector('.img').getElementsByTagName('img')[0].style.width="40vh";
+                music.play();
                 isgameover=true;
-                document.querySelector('.info').innerText=boxtext.e[0]+"won";
         }
     });
 }
 
-
+document.querySelector('.info').innerHTML='Turn for '+turn;
 let boxes=document.getElementsByClassName("box");//all the box classes are selected
 
 //turning all of them into array and accessing them with for each loop
@@ -50,8 +56,7 @@ Array.from(boxes).forEach(element =>{
             boxtext.innerText=turn;//default turn is X
             turnaudio.play();//this sound will play
             turn=changeturn();//after putting X turn will change to 0
-            // checkwin();
-
+            chackwin();
             if(!isgameover){
                 document.querySelector('.info').innerHTML='Turn for '+turn;
             }
@@ -59,3 +64,38 @@ Array.from(boxes).forEach(element =>{
         }
     });
 });
+
+let boxtext=document.querySelectorAll(".boxtext");
+let reset=document.querySelector('button');
+Array.from(boxtext).forEach(element =>{
+    reset.addEventListener('click',()=>{
+        element.innerText="";
+        turn="X";
+        document.querySelector('.info').innerHTML='Turn for '+turn;
+        music.pause();
+        document.querySelector('.img').getElementsByTagName('img')[0].style.width="0";
+
+    });
+   
+
+});
+
+
+
+
+
+// queries
+// queries
+
+
+
+
+
+
+
+
+
+
+// queries
+// queries
+
